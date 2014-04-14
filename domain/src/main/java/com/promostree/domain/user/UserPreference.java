885b46f69375c01c0e768b44b0f55d2d6bf74571
swaroop.k <swaroop.k@visigenix.com>
2014-04-14 15:00:49 +0530

    restructure

---
+++ domain/src/main/java/com/promostree/domain/user/UserPreference.java
@@ -0,0 +1,69 @@
+package com.promostree.domain.user;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.codehaus.jackson.annotate.JsonBackReference;
+import org.codehaus.jackson.annotate.JsonManagedReference;
+@Entity(name="user_preference")
+@Table(name="user_preference")
+public class UserPreference
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+private Long id;
+
+@OneToOne
+@JoinColumn(name="preferenceTypeId")
+@JsonManagedReference
+private Type type;
+
+
+private String value;
+
+@ManyToOne
+@JoinColumn(name="userId")
+@JsonBackReference
+private User user;
+
+
+public User getUser() {
+	return user;
+}
+public void setUser(User user) {
+	this.user = user;
+}
+public Long getId() {
+	return id;
+}
+public void setId(Long id) {
+	this.id = id;
+}
+
+
+
+public Type getType() {
+	return type;
+}
+public void setType(Type type) {
+	this.type = type;
+}
+public String getValue() {
+	return value;
+}
+public void setValue(String value) {
+	this.value = value;
+}
+
+
+
+
+
+}
