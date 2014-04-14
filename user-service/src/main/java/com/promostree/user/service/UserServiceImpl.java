c575974c71ecff9d8a62494d32bf9b98fa13e9a5
mallikarjun <mallikarjun0535@gmail.com>
2014-04-14 16:28:00 +0530

    to pull

--- user-service/src/main/java/com/promostree/user/service/UserServiceImpl.java
+++ user-service/src/main/java/com/promostree/user/service/UserServiceImpl.java
@@ -2,10 +2,8 @@ package com.promostree.user.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 import com.promostree.domain.entities.Location;
