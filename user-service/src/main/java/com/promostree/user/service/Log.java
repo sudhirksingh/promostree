1e78f2c3c3398edc1f6f7dc97a2b95df1f9a6588
swaroop.k <swaroop.k@visigenix.com>
2014-05-15 14:28:26 +0530

    nothing done

---
+++ user-service/src/main/java/com/promostree/user/service/Log.java
@@ -0,0 +1,19 @@
+package com.promostree.user.service;
+
+import java.util.List;
+
+import com.promostree.domain.user.User;
+
+public class Log {
+private List<User> users;
+
+public List<User> getUsers() {
+	return users;
+}
+
+public void setUsers(List<User> users) {
+	this.users = users;
+}
+
+
+}
